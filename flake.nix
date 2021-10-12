7b6b880f8cc251171a835289e3a34d57701bd905
Graham Christensen <graham@grahamc.com>
2021-10-12 13:48:35 -0400

    Basic Nix flake with Terraform

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Building GitHub PRs in Hydra.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs =
+    { nixpkgs
+    , self
+    , ...
+    }@inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+
+      pkgsFor = pkgs: system:
+        import pkgs { inherit system; };
+
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      forAllSystems = f: genAttrs allSystems
+        (system: f {
+          inherit system;
+          pkgs = pkgsFor nixpkgs system;
+        });
+    in
+    {
+      pkgs = forAllSystems ({ pkgs, ... }: pkgs);
+
+      devShell = forAllSystems
+        ({ pkgs, ... }:
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nixpkgs-fmt
+              shellcheck
+              (terraform_1_0.withPlugins (p: [
+                p.hydra
+              ]))
+            ];
+          }
+        );
+    };
+}
