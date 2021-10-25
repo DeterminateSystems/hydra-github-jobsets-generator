099d717cf417794341b86ee17290714e5a49671f
Graham Christensen <graham@grahamc.com>
2021-10-25 11:03:59 -0400

    Accept arguments in the input.json

--- jobset-generator/src/cli.rs
+++ jobset-generator/src/cli.rs
@@ -1,5 +1,6 @@
-use std::{collections::BTreeMap, fs::File, io::BufReader};
-
+use std::{fs::File, io::BufReader};
+use serde;
+use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
 
 use crate::Result;
@@ -12,53 +13,86 @@ use crate::{
 #[derive(StructOpt)]
 struct Args {
     pull_requests_file: String,
-    #[structopt(long, conflicts_with = "flakes")]
-    template: Option<String>,
-    #[structopt(long, conflicts_with = "template")]
+    config_file: String,
+}
+
+#[derive(Serialize,Deserialize)]
+#[serde(deny_unknown_fields)] 
+struct GeneratorConfig {
+    #[serde(default)]
     flakes: bool,
-    #[structopt(long, default_value = "300")]
+
+    #[serde(default = "default_check_interval")]
     check_interval: u64,
-    #[structopt(long, default_value = "1")]
+
+    #[serde(default = "default_scheduling_shares")]
     scheduling_shares: u64,
-    #[structopt(long)]
+
+    #[serde(default)]
     email_enable: bool,
-    #[structopt(long, default_value = "")]
-    email_override: String,
-    #[structopt(long)]
+
+    email_override: Option<String>,
+
+    #[serde(default)]
     email_responsible: bool,
-    #[structopt(long, default_value = "3")]
+
+    #[serde(default = "default_keep_evaluations")]
     keep_evaluations: u64,
-    #[structopt(long, default_value = "src")]
+
+    #[serde(default = "default_input_name")]
     input_name: String,
-    #[structopt(long, default_value = "input_path")]
+
+    #[serde(default = "default_input_path")]
     input_path: String,
+
+    #[serde(default)]
+    inputs: JobInputCollection
+}
+
+fn default_check_interval() -> u64 {
+    300
+}
+
+fn default_scheduling_shares() -> u64 {
+    1
+}
+
+fn default_keep_evaluations() -> u64 {
+    3
+}
+
+fn default_input_name() -> String {
+    String::from("src")
+}
+
+fn default_input_path() -> String {
+    String::from("default.nix")
 }
 
 pub fn cli() -> Result<()> {
     let args = Args::from_args();
 
-    let input_template: JobInputCollection = if let Some(template) = args.template {
-        let template_file = File::open(template)?;
-        let template_rdr = BufReader::new(template_file);
-
-        serde_json::from_reader(template_rdr)?
-    } else {
-        BTreeMap::new()
+    let config: GeneratorConfig = {
+        let config_file = File::open(args.config_file)?;
+        serde_json::from_reader(BufReader::new(config_file))?
     };
+
+    eprintln!("Expanded the provided configuration input to:");
+    eprintln!("{}", serde_json::to_string_pretty(&config).unwrap());
 
     let job_config = JobConfig {
-        checkinterval: args.check_interval,
-        emailoverride: args.email_override,
-        enableemail: args.email_enable,
-        email_responsible: args.email_responsible,
-        inputname: args.input_name,
-        inputpath: args.input_path,
-        keepnr: args.keep_evaluations,
-        schedulingshares: args.scheduling_shares,
-        input_template,
+        checkinterval: config.check_interval,
+        emailoverride: config.email_override.unwrap_or_default(),
+        enableemail: config.email_enable,
+        email_responsible: config.email_responsible,
+        inputname: config.input_name,
+        inputpath: config.input_path,
+        keepnr: config.keep_evaluations,
+        schedulingshares: config.scheduling_shares,
+        input_template: config.inputs,
     };
 
-    let make_definition = if args.flakes {
+    let make_definition = if config.flakes {
         make_flake_definition
     } else {
         make_legacy_definition
@@ -70,4 +104,14 @@ pub fn cli() -> Result<()> {
     println!("{}", json);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::GeneratorConfig;
+
+    #[test]
+    fn can_decode_empty_config() {
+        let _cfg: GeneratorConfig = serde_json::from_str("{}").expect("Failed to decode all-defaults");
+    }
 }
